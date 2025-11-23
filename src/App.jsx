ea880376d2b9fb365449323ce58b7b57c5ba4aa8
vijay Randhave <vijayrandhave77@gmail.com>
2025-11-23 17:50:50 +0530

    version v2

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
           <Route path="/" element={
             <>
               <Hero />
-              <About />
+              <About /> 
               <Services />
               <Portfolio />
               <Technologies />
